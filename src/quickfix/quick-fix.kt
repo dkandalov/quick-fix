c939c751780f3d2e8c59dcbccae8716e4ef4d78b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-05-12 07:51:57 +0100

    configurable quickfix intention priorities

--- src/quickfix/quick-fix.kt
+++ src/quickfix/quick-fix.kt
@@ -1,19 +1,22 @@
-@file:Suppress("UnstableApiUsage")
-
 package quickfix
 
 import com.intellij.codeInsight.intention.*
 import com.intellij.codeInsight.intention.impl.CachedIntentions
+import com.intellij.codeInsight.intention.impl.IntentionActionWithTextCaching
 import com.intellij.codeInsight.intention.impl.ShowIntentionActionsHandler
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.impl.DynamicActionConfigurationCustomizer
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.util.registry.RegistryValue
+import com.intellij.openapi.util.registry.RegistryValueListener
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
@@ -26,12 +29,34 @@ class QuickFixAction : AnAction() {
         val psiFile = project.currentPsiFile ?: return
 
         // Based on com.intellij.analysis.problemsView.toolWindow.ShowQuickFixesAction
+        @Suppress("UnstableApiUsage")
         val intentionsInfo = ShowIntentionActionsHandler.calcIntentions(project, editor, psiFile)
         val cachedIntentions = CachedIntentions.createAndUpdateActions(project, psiFile, editor, intentionsInfo)
-        val fix = cachedIntentions.allActions.firstOrNull { it.action.canBeInvoked() } ?: return
+        val fix = cachedIntentions.allActions.sortedBy { it.quickFixPriority() }
+            .firstOrNull { it.action.canBeInvoked() } ?: return
 
         val commandName = StringUtil.capitalizeWords(fix.action.text, true)
         ShowIntentionActionsHandler.chooseActionAndInvoke(psiFile, editor, fix.action, commandName)
+    }
+
+    private fun IntentionActionWithTextCaching.quickFixPriority(): Int =
+        intentionPriorities[action.text] ?: (intentionPriorities["*"] ?: -1)
+
+    companion object {
+        private val registryValue: RegistryValue = Registry.get("quickfix-plugin.intentionPriorities").also {
+            it.addListener(object : RegistryValueListener {
+                override fun afterValueChanged(value: RegistryValue) {
+                    intentionPriorities = it.toIntentionPriorityMap()
+                }
+            }, ApplicationManager.getApplication())
+        }
+
+        private fun RegistryValue.toIntentionPriorityMap() =
+            asString().split(";")
+                .mapIndexed { index, value -> value to index }
+                .toMap()
+
+        private var intentionPriorities = registryValue.toIntentionPriorityMap()
     }
 }
 
