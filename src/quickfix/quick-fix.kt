7a0117dbf35241e8b11ff922e8cb7f833e9ccc73
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-07-19 09:16:21 +0100

    fixed intentions for quickfix being sorted differently than in UI popup

--- src/quickfix/quick-fix.kt
+++ src/quickfix/quick-fix.kt
@@ -33,13 +33,20 @@ class QuickFixAction : AnAction() {
         val intentionsInfo = ShowIntentionActionsHandler.calcIntentions(project, editor, psiFile)
         val cachedIntentions = CachedIntentions.createAndUpdateActions(project, psiFile, editor, intentionsInfo)
 
-        val fix = cachedIntentions.allActions
-            // Sort all intentions separately so that e.g. errors always come first regardless of sorting.
-            .reorderSublist(cachedIntentions.errorFixes.sortedBy { it.quickFixPriority() })
-            .reorderSublist(cachedIntentions.inspectionFixes.sortedBy { it.quickFixPriority() })
-            .reorderSublist(cachedIntentions.intentions.sortedBy { it.quickFixPriority() })
-            .reorderSublist(cachedIntentions.gutters.sortedBy { it.quickFixPriority() })
-            .reorderSublist(cachedIntentions.notifications.sortedBy { it.quickFixPriority() })
+        // The code below is a bit convoluted because it's trying to:
+        //  - keep intentions overall in the same order as in UI, i.e. as returned by `cachedIntentions.allActions`
+        //  - keep intentions within the same categories, i.e. errors always come first regardless of additional sorting
+        //  - reorder subset of intentions according to QuickFix plugin priorities
+        // (Note that an "obvious" solution to sort all intentions using the same sorting as `cachedIntentions.allActions`
+        // and then applying QuickFix sort won't work, because in this case QuickFix sort can put intentions above errors, etc.
+        // This will also require copy-pasting private sorting code from CachedIntentions.)
+        val allActions = cachedIntentions.allActions
+        val fix = allActions
+            .reorderSublist(allActions.subsetOf(cachedIntentions.errorFixes).sortedBy { it.quickFixPriority() })
+            .reorderSublist(allActions.subsetOf(cachedIntentions.inspectionFixes).sortedBy { it.quickFixPriority() })
+            .reorderSublist(allActions.subsetOf(cachedIntentions.intentions).sortedBy { it.quickFixPriority() })
+            .reorderSublist(allActions.subsetOf(cachedIntentions.gutters).sortedBy { it.quickFixPriority() })
+            .reorderSublist(allActions.subsetOf(cachedIntentions.notifications).sortedBy { it.quickFixPriority() })
             .firstOrNull { it.action.canBeInvoked() } ?: return
 
         val commandName = StringUtil.capitalizeWords(fix.action.text, true)
@@ -138,6 +145,8 @@ private val AnActionEvent.currentPsiFile: PsiFile?
 
 private val AnActionEvent.currentEditor: Editor?
     get() = getData(EDITOR)
+
+private fun <T> List<T>.subsetOf(set: Set<T>): List<T> = filter { it in set }
 
 fun <T> List<T>.reorderSublist(order: List<T>): List<T> {
     if (order.isEmpty()) return this
