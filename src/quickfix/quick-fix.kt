2a3622bb9de69fee969270d48c8a94535ff5efb2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-12-16 21:19:47 +0000

    removed AddIntentionActions because it's now part of IDE (issue #3)

--- src/quickfix/quick-fix.kt
+++ src/quickfix/quick-fix.kt
@@ -4,13 +4,10 @@ import com.intellij.codeInsight.intention.*
 import com.intellij.codeInsight.intention.impl.CachedIntentions
 import com.intellij.codeInsight.intention.impl.IntentionActionWithTextCaching
 import com.intellij.codeInsight.intention.impl.ShowIntentionActionsHandler
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ActionUpdateThread.BGT
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys.EDITOR
 import com.intellij.openapi.actionSystem.CommonDataKeys.PSI_FILE
-import com.intellij.openapi.actionSystem.impl.DynamicActionConfigurationCustomizer
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.registry.Registry
@@ -72,68 +69,6 @@ class QuickFixAction : AnAction() {
 
         private fun IntentionActionWithTextCaching.quickFixPriority() =
             intentionPriorities[action.text] ?: (intentionPriorities["*"] ?: -1)
-    }
-}
-
-class AddIntentionActions : DynamicActionConfigurationCustomizer {
-    private val actions by lazy {
-        IntentionManager.getInstance().intentionActions
-            .filter { it.canBeInvoked() }
-            // Group by name because there are intentions with the same name,
-            // e.g. "Put arguments on separate lines" for Java and Kotlin.
-            .groupBy { it.familyName }
-            .map { (familyName, intentionActions) ->
-                val actionId = "$familyName (Intention)"
-                IntentionAsAction(actionId, intentionActions)
-            }
-    }
-
-    override fun registerActions(actionManager: ActionManager) {
-        actions.forEach {
-            actionManager.registerAction(it.actionId, it)
-        }
-    }
-
-    override fun unregisterActions(actionManager: ActionManager) {
-        actions.forEach {
-            actionManager.unregisterAction(it.actionId)
-        }
-    }
-}
-
-private class IntentionAsAction(
-    val actionId: String,
-    private val intentionActions: List<IntentionAction>
-) : AnAction(actionId) {
-    override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project ?: return
-        val editor = event.currentEditor ?: return
-        val psiFile = event.currentPsiFile ?: return
-
-        intentionActions.forEach {
-            if (it.isAvailable(project, editor, psiFile)) {
-                val commandName = StringUtil.capitalizeWords(it.text, true)
-                ShowIntentionActionsHandler.chooseActionAndInvoke(psiFile, editor, it, commandName)
-            }
-        }
-    }
-
-    override fun getActionUpdateThread() = BGT
-
-    override fun update(event: AnActionEvent) {
-        // There are few intentions that perform similar transformations and can benefit from having the same shortcut
-        // (for example, "Put arguments on one line" and "Put parameters on one line" in Kotlin).
-        // The code below sets action enabled status to make sure that the shortcut invokes the action
-        // which is actually available in the context.
-        // Otherwise, the action which is not available in the current context might be invoked and it will be a noop.
-        event.presentation.isEnabled = hasAvailableActions(event)
-    }
-
-    private fun hasAvailableActions(event: AnActionEvent): Boolean {
-        val project = event.project ?: return false
-        val editor = event.currentEditor ?: return false
-        val psiFile = event.currentPsiFile ?: return false
-        return intentionActions.any { it.isAvailable(project, editor, psiFile) }
     }
 }
 
